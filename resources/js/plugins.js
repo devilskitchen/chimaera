8370a485b360996f86f1b025abdacd87acf594b3
Chris Mounsey <chris.mounsey@verseone.com>
2013-12-26 17:11:59 +0000

    Started to assemble the framework
    Pulling files from multiple projects. This might take a while to
    reconcile.

---
+++ resources/js/plugins.js
@@ -0,0 +1,50 @@
+// Avoid `console` errors in browsers that lack a console.
+(function() {
+    var method;
+    var noop = function () {};
+    var methods = [
+        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+        'timeStamp', 'trace', 'warn'
+    ];
+    var length = methods.length;
+    var console = (window.console = window.console || {});
+
+    while (length--) {
+        method = methods[length];
+
+        // Only stub undefined methods.
+        if (!console[method]) {
+            console[method] = noop;
+        }
+    }
+}());
+
+// Place any jQuery/helper plugins in here.
+
+// Add classes when elements become visible through scolling (provided by js/lib/jquery.visible.min.js): CJDM, 12/12/13
+// With thanks to Chris Coyier: http://css-tricks.com/slide-in-as-you-scroll-down-boxes/
+
+var win = $(window);
+var allMods = $(".module");
+
+// Already visible modules
+allMods.each(function(i, el) {
+  var el = $(el);
+  if (el.visible(true)) {
+    el.addClass("already-visible"); 
+  } 
+});
+
+win.scroll(function(event) {
+  
+  allMods.each(function(i, el) {
+    var el = $(el);
+    if (el.visible(true)) {
+      el.addClass("come-in"); 
+    } 
+  });
+  
+});
+
